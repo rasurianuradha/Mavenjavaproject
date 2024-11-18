929dae0e53bd8357bd309ebe0a7788417dbf3cb5
rasurianuradha <anu057821@gmail.com>
2024-11-18 22:55:40 +0530

    First commit to maven java project

---
+++ src/main/java/SEGroup/Mavenjavaprj/App.java
@@ -0,0 +1,13 @@
+package SEGroup.Mavenjavaprj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello all............." );
+    }
+}
